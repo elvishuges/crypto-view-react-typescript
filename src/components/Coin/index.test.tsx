cf27bc3e477bd225e75c81470e00c24ca8f3eee4
elvishuges <elvishuges@hotmail.com>
2020-11-07 23:26:13 -0300

    add arquivos de configuracao para deploy ci cont. integration

--- src/components/Coin/index.test.tsx
+++ src/components/Coin/index.test.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import { Coin } from './index'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Coin } from './index';
 
-it("Check coin Label", () => {
-    render(<Coin coin="BTC" oldPrice={0} currentPrice={0} />)
-    expect(screen.getByText("BTC"))
+it('Check coin label', () => {
+    render(<Coin coin="BTC" currentPrice={0} oldPrice={0} />)
+    expect(screen.getByText('BTC'));
 })
